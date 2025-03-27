7f9bd57a39f463d052eb2e657de157d15503efad
ofthenorth <58706076+ofthenorth@users.noreply.github.com>
2025-03-27 15:29:16 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+// service-worker.js
+const CACHE_NAME = 'norwegian-4x4-timer';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
